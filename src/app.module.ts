12fe959f7819c571ba9fe69ed0d52c586ef3e0aa
kerAymen <6246005@collegemv.qc.ca>
2026-09-13 17:13:33 -0400

    ajout places, reviews

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlacesModule } from './places/places.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [PlacesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
